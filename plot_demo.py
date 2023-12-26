9b96ac222e6336bf8f8aef6eacfb3ed760833a51
Daniel Chong Sánchez <46977330+dc36205@users.noreply.github.com>
2023-12-26 11:54:34 -0800

    Add files via upload

---
+++ plot_demo.py
@@ -0,0 +1,29 @@
+# Streamlit plotting demo
+
+# Steps
+#1. Make a Python file where we will house all our Streamlit code.
+#2. Use the plotting code given in the demo.
+#3. Make small edits for practice.
+#4. Run our file locally .
+
+import streamlit as st
+import time
+import numpy as np
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+fori in range(1, 101):
+new_rows = last_rows[-1, :] + np.random.ran
+status_text.text("%i%% Complete"% i)
+chart.add_rows(new_rows)
+progress_bar.progress(i)
+last_rows = new_rows
+time.sleep(0.05)
+progress_bar.empty()
+
+# Streamlit widgets automatically run the scrip
+# this button is not connected to any other log
+# rerun.
+st.button("Re-run")
