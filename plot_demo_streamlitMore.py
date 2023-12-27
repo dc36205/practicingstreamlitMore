243ab21c6716b65c672eff9fb1fbd1992bd8ee76
Daniel <dc36205@gmail.com>
2023-12-27 14:49:53 -0800

    Moving to a new folder

--- plot_demo_streamlitMore.py
+++ plot_demo_streamlitMore.py
@@ -15,7 +15,7 @@ progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
-fori in range(1, 101):
+for i in range(1, 101):
 new_rows = last_rows[-1, :] + np.random.ran
 status_text.text("%i%% Complete"% i)
 chart.add_rows(new_rows)
@@ -27,4 +27,4 @@ progress_bar.empty()
 # Streamlit widgets automatically run the scrip
 # this button is not connected to any other log
 # rerun.
-st.button("Re-run")
+st.button("Re-run") # Boton al finallllllll!!!!
